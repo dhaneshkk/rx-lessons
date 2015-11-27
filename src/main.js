5a0fcde86310229fc00756c53cd84740cabf35a4
dhaneshkk <dhaneshkk@gmail.com>
2015-11-27 21:09:27 +0530

    adding rx code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
+import Rx from './rx.min.js';
+/*var source = Rx.Observable.create(observer => {
+  // Yield a single value and complete
+  observer.onNext(42);
+  observer.onCompleted();
+
+  // Any cleanup logic might go here
+  return () => console.log('disposed')
+});
+*/
+var source = Rx.Observable.range(1, 5);
+
+
+var subscription = source.subscribe(
+  x => console.log('onNext: %s', x),
+  e => console.log('onError: %s', e),
+  () => console.log('onCompleted'));
+
+// => onNext: 42
+// => onCompleted
+
+subscription.dispose();
+// => disposed
 
